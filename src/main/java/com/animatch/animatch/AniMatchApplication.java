ac086d9d09edd5611f0f31327414daed128aad0a
tamanghawana09 <hawanatamang@gmail.com>
2026-03-15 10:23:00 +0545

    initial commit

---
+++ src/main/java/com/animatch/animatch/AniMatchApplication.java
@@ -0,0 +1,13 @@
+package com.animatch.animatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AniMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/animatch/animatch/AniMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animatch.animatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
