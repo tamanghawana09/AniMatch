aa3d39813d82e4073b16f38d3b6d500b894043f1
tamanghawana09 <hawanatamang@gmail.com>
2026-03-22 17:42:32 +0545

    version: updated version

--- src/main/java/com/animatch/animatch/DTO/AiringAnimeDTO.java
+++ src/main/java/com/animatch/animatch/DTO/AiringAnimeDTO.java
@@ -3,7 +3,6 @@ package com.animatch.animatch.DTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import tools.jackson.databind.annotation.JsonAppend;
 
 import java.util.ArrayList;
 import java.util.Date;
